061d7042a404029b2fcc54174a8c1f17cd3670b2
larys <larys.quistin@edu.ece.fr>
2020-04-18 18:30:31 +0200

    Create Ventes.php

---
+++ Ventes.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    
+    <?php include 'includes/head.php'; ?>
+
+    <body>
+        <?php include 'includes/header.php'; ?>
+
+        <div class="container features">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <h3 class="feature-title">Achat Immédiat</h3>
+                    <a class="Img_Cat" href="achat_immediat.php">
+                        <img src="images\ventes\achat_immediat.png" width="400" height="400" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                <h3 class="feature-title">Encheres</h3>
+                    <a class="Img_Cat" href="enchere.php">
+                        <img src="images\ventes\encheres.png" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                <h3 class="feature-title">Achat immediat ou meilleure offre</h3>
+                    <a class="Img_Cat" href="achat_offre">
+                        <img src="images\ventes\achat_offre.png" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <br></br>
+        <br></br>
+        <br></br>
+
+
+        <?php include 'includes/footer.php'; ?>
+
+    </body>
+
+
+</html>
