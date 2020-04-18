e3dc4a58eaf5ac028628d07039a7fa2a70b2d081
larys <larys.quistin@edu.ece.fr>
2020-04-18 18:32:57 +0200

    Update bon_pour_le_musee.php

--- bon_pour_le_musee.php
+++ bon_pour_le_musee.php
@@ -11,35 +11,40 @@
 
         $database = "ebay_ece";
 
-        $db_handle = mysqli_connect('127.0.0.1:3308', 'root', '');
+        $db_handle = mysqli_connect('127.0.0.1:3306', 'root', 'root');
         $db_found = mysqli_select_db($db_handle, $database);
 
         if ($db_found) {
             $sql = "SELECT * FROM item WHERE Categorie LIKE 'musee'"; 
             $result = mysqli_query($db_handle, $sql);
 
+
             ?>
             <div>
             <div class="container">
+
             <h3 class="feature-title" align="center">Bon pour le musée</h3><br>
 
             <div class="row">
 
             <?php while ($data = mysqli_fetch_assoc($result)) { ?>
                 <div class="col-sm-4">
-                    <a style="text-decoration:none" href="fiche_article.php">
-                        <div class="panel panel-primary">
+
+                    <div class="panel panel-primary"> 
+                    <a style="text-decoration:none" <?php echo 'href="fiche_article.php?id='.$data["IdItem"].'"';?> >
                             <div class="panel-heading" align="center"><?php echo $data['Nom']; ?></div><br>
-                            <div class="panel-body" align="center"><?php $photo = $data['Photo1'];
-                            echo "<img src='$photo' class='img-responsive' style='width:50%' alt='photo_1'>"; ?></div><br>
-                            <div class="panel-footer" align="center">
-                            <?php if ($data['TypeAchat'] == "immediat_offre") { echo 'Achat immédiat ou par meilleure offre'; }
-                            if ($data['TypeAchat'] == "immediat") { echo 'Achat immédiat'; }
-                            if ($data['TypeAchat'] == "offre") { echo 'Achat par meilleure offre'; }
-                            if ($data['TypeAchat'] == "enchere") { echo 'Achat par enchères'; } ?><br>
-                            <?php $prix = number_format($data['Prix'], 2, ',', ' '); echo $prix . " €"; ?></div><br>
-                        </div>
-                    </a>
+                                <div class="panel-body" align="center"><?php $photo = $data['Photo1'];
+                                  echo "<img src='$photo' class='img-responsive' style='width:50%' alt='photo_1'>"; ?></div><br> </a>
+                                    <div class="panel-footer" align="center">
+                                 <?php if ($data['TypeAchat'] == "immediat_offre") { echo 'Achat immédiat ou par meilleure offre'; }
+                                       if ($data['TypeAchat'] == "immediat") { echo 'Achat immédiat'; }
+                                       if ($data['TypeAchat'] == "offre") { echo 'Achat par meilleure offre'; }
+                                       if ($data['TypeAchat'] == "enchere") { echo 'Achat par enchères'; } ?><br>
+                                       <?php $_SESSION["item"]=$data["IdItem"];?>
+                                       <?php $prix = number_format($data['Prix'], 2, ',', ' '); echo $prix . " €"; ?></div><br>
+                
+                       </div>
+                   
                 </div>
             <?php
             }
