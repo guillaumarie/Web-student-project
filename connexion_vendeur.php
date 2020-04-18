823571bbc2fb7de67e5680f478182b4bcf33becc
larys <larys.quistin@edu.ece.fr>
2020-04-18 18:32:39 +0200

    Update connexion_vendeur.php

--- connexion_vendeur.php
+++ connexion_vendeur.php
@@ -10,7 +10,7 @@ session_start();
     if($email && $password) {
         $database = "ebay_ece";
 
-        $db_handle = mysqli_connect('127.0.0.1:3308', 'root', '');
+        $db_handle = mysqli_connect('127.0.0.1:3306', 'root', 'root');
         $db_found = mysqli_select_db($db_handle, $database);
 
         if ($db_found) {
@@ -36,7 +36,7 @@ session_start();
             echo "Database not found.<br>";
         }
         mysqli_close($db_handle);
-        header('Location: espace_vendeur.php');
+        header('Location: accueil.php');
     } else {
         echo "Veuillez remplir tous les champs.<br>";
     }
